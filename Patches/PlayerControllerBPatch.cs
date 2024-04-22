e41541dcae2da561d74c41c5894aa49833f20f28
MishAnya <MishAnya@DESKTOP-N1S0S6V>
2024-04-22 17:05:33 +0300

    Add project files.

---
+++ Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using GameNetcodeStuff;
+using System;
+
+namespace MyMod.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void InfiniteSprintPatch(PlayerControllerB __instance)
+        {
+            __instance.sprintMeter = 1f;
+        }
+
+        [HarmonyPatch("movementSpeed")]
+        [HarmonyPostfix]
+        static void MovementSpeedPatch(PlayerControllerB __instance)
+        {
+            __instance.movementSpeed = 2f;
+        }
+
+        [HarmonyPatch("climbSpeed")]
+        [HarmonyPostfix]
+        static void ClimbSpeedPatch(PlayerControllerB __instance)
+        {
+            __instance.climbSpeed = 8f;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using MyMod.Patches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMod
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class ModBase : BaseUnityPlugin
+    {
+        private const string modGUID = "FirstMod.JustMod";
+        private const string modName = "Just Speed Buffs";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static ModBase Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+
+            mls.LogInfo("This shit is working :D");
+
+            harmony.PatchAll(typeof(ModBase));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+        }
+    }
+}
